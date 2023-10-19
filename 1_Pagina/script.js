c3f1970e697e88a4cb987a918220d4d71226a496
CaioLira18 <caiozao1212@gmail.com>
2023-10-18 22:53:20 -0300

    Site Atualizado

---
+++ 1_Pagina/script.js
@@ -0,0 +1,6 @@
+var nomeUsuario = prompt("Digite seu nome para utilizar na plataforma?", "Nome");
+if(nomeUsuario != '') {
+ alert("Seu nome é: "+nomeUsuario+"!");
+} else {
+ alert("Pôxa, nem sei qual é seu nome =/");
+}
