400789e4b71209236cd622db53ae544a9a86e057
CaioLira18 <caiozao1212@gmail.com>
2023-11-09 18:56:40 -0300

    Dashboard

--- 1_Pagina/script.js
+++ 1_Pagina/script.js
@@ -1,6 +1,13 @@
 var nomeUsuario = prompt("Digite seu nome para utilizar na plataforma?", "Nome");
-if(nomeUsuario != '') {
- alert("Seu nome é: "+nomeUsuario+"!");
+
+if (nomeUsuario != '') {
+    alert("Seu nome é: " + nomeUsuario + "!");
+    
 } else {
- alert("Pôxa, nem sei qual é seu nome =/");
+    alert("Pôxa, nem sei qual é seu nome =/");
 }
+
+document.write("Seu nome: " + nomeUsuario);
+
+
+
